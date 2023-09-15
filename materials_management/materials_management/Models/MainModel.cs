a305a91de98736da635e4dbf13e6de7a84499df6
wan <106522971+wan0911@users.noreply.github.com>
2023-09-15 20:32:58 +0900

    콤보박스 데이터바인딩

---
+++ materials_management/materials_management/Models/MainModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace materials_management.Models
+{
+    public class MainModel : INotifyPropertyChanged
+    {
+
+        private string materialGroupCode;            // 자재그룹코드
+        public string MaterialGroupCode { 
+            set { materialGroupCode = value; }
+            get {  return materialGroupCode; }
+        }
+
+
+        private string materialGroupName;           // 자재그룹명
+        public string MaterialGroupName {
+            set { materialGroupName = value; }
+            get { return materialGroupName; }
+        }
+
+        private string materialGroupSelection;           // 사용여부
+        public string MaterialGroupSelection {
+            set { MaterialGroupSelection = value; }
+            get { return MaterialGroupSelection; }
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ materials_management/materials_management/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using materials_management.Models;
+
+namespace materials_management.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Models.MainModel model = null;
+
+        //public Models.MainModel Model
+        //{
+
+        //}
+        
+        public MainViewModel ()
+        {
+            model = new Models.MainModel ();    
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
